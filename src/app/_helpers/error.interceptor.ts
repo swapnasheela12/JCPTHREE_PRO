c63433ef0da2692f43e81ffdde2ab9442632032c
Swapnasheela1.K <Swapnasheela1.K@ril.com>
2020-05-04 11:05:19 +0530

    my report

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError,tap } from 'rxjs/operators';
 
 import { AuthenticationService } from '../_services/authentication.service';
 
@@ -10,12 +10,15 @@ export class ErrorInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
+        return next.handle(request).pipe(tap(data => console.log(data)),catchError(err => {
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
                 this.authenticationService.logout();
                 location.reload(true);
             }
+            // else if(error.error instanceof ErrorEvent){
+            //     console.error('An error occurred:', error.error.message);
+            // }
 
             const error = err.error.message || err.statusText;
             return throwError(error);
