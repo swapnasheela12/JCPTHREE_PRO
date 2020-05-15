220062300de968bd47581fb44e019c4730064e66
Swapnasheela1.K <Swapnasheela1.K@ril.com>
2020-05-15 16:26:11 +0530

    home screen

--- src/app/home-jcp-three/landing-home/landing-home.component.ts
+++ src/app/home-jcp-three/landing-home/landing-home.component.ts
@@ -14,11 +14,38 @@ export class LandingHomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  sitesListArr = [{
-    icon: "",
-    name: "",
-    countsite: ""
-  }];
+  sitesListArr = [
+    {
+    iconsite: "ic ic-Macro-Sites",
+    namesite: "Macro Sites",
+    countsite: "289137"
+  },
+    {
+    iconsite: "ic ic-small-Cells",
+    namesite: "Small Cells",
+    countsite: "293456"
+  },
+    {
+    iconsite: "ic ic-ESC",
+    namesite: "ESC",
+    countsite: "2345"
+  },
+    {
+    iconsite: "ic ic-COW",
+    namesite: "COW",
+    countsite: "2345"
+  },
+    {
+    iconsite: "ic ic-FTTX",
+    namesite: "FTTX",
+    countsite: "122345"
+  },
+    {
+    iconsite: "ic ic-Wifi",
+    namesite: "WI-FI APS",
+    countsite: "271420"
+  },
+];
 
 
 }
