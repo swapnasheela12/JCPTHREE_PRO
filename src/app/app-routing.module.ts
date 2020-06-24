97c793273a4258efb88da987a5290b0204f2eda4
Dhairya Shree <dhairya.shree@ril.com>
2020-06-24 21:13:39 +0530

    committing missed file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,21 @@ import { ReportsWizardComponent } from './main-modules/reports-dashboards/report
 import { MyReportsComponent } from './main-modules/reports-dashboards/my-reports/my-reports.component';
 import { LandingHomeComponent } from './home-jcp-three/landing-home/landing-home.component';
 import { MyJcpComponent } from './main-modules/my-jcp/my-jcp.component';
+import { RowRenderingComponent } from './modules/components/row-rendering/row-rendering.component';
+import { ColumnRenderingComponent } from './modules/components/column-rendering/column-rendering.component';
+import { TreeRenderingComponent } from './modules/components/tree-rendering/tree-rendering.component';
 // import { MainLayerComponent } from './main-layer/main-layer.component';
 
 const routes: Routes = [
- 
   { path: "", component: LoginJcpThreeComponent },
   {
     path: "JCP", component: HomeJcpThreeComponent,
     canActivate: [AuthGuard],
     children: [
       { path: "Home", component: LandingHomeComponent },
-      // { path: "Layers", component:LayerMainComponent},
-      // { path: "Layers", component:MainLayerComponent},
+      { path: "Row-Rendering", component:RowRenderingComponent},
+      { path: "Column-Rendering", component:ColumnRenderingComponent},
+      { path: "Tree-Rendering", component:TreeRenderingComponent},
       { path: "My-JCP", component:MyJcpComponent},
       { path: "Layers", loadChildren: "../app/main-modules/main-modules.module#MainModulesModule"},
       { path: "Reports-and-Dashboard", loadChildren: "../app/main-modules/main-modules.module#MainModulesModule" },
