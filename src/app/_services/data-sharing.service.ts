f6e1564d4fb7d1680c0ded2fc0a6dd8e121802c3
Dhairya Shree <dhairya.shree@ril.com>
2020-07-10 11:31:07 +0530

    Changes for kpi editor

--- src/app/_services/data-sharing.service.ts
+++ src/app/_services/data-sharing.service.ts
@@ -7,10 +7,17 @@ export class DataSharingService {
   private messageSource = new BehaviorSubject({});
   currentMessage = this.messageSource.asObservable();
 
+  private checkBoxSource = new BehaviorSubject({});
+  checkboxMessage = this.checkBoxSource.asObservable();
+
   constructor() { }
 
   changeMessage(messages: Object) {
     this.messageSource.next(messages);
+  }
+
+  chechboxChangeMessage(checkBox: Object) {
+    this.checkBoxSource.next(checkBox);
 
   }
 }
