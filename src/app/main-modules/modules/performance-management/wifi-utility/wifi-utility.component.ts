19378309c265fa2fbb08354c875934285bc88f12
Abhishek6.C <Abhishek6.C@LT0105643.in.ril.com>
2020-07-15 12:05:23 +0530

    WIFI UTILITY ASSET FILES.

---
+++ src/app/main-modules/modules/performance-management/wifi-utility/wifi-utility.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wifi-utility',
+  templateUrl: './wifi-utility.component.html',
+  styleUrls: ['./wifi-utility.component.scss']
+})
+export class WifiUtilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
