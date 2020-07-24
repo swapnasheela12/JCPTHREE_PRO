5d89f73257f164d92888f50467eb1418bdde1c05
Swapnasheela1.K <Swapnasheela1.K@ril.com>
2020-07-24 21:30:53 +0530

    report builder

--- src/app/main-modules/main-layer/legends-and-filter/legends-and-filter.component.ts
+++ src/app/main-modules/main-layer/legends-and-filter/legends-and-filter.component.ts
@@ -34,16 +34,16 @@ export class LegendsAndFilterComponent implements OnInit {
       { value: -90, legend: '' },
       { value: -40, legend: '' },
     ],
-    // translate: (value: number, label: LabelType): string => {
-    //   switch (label) {
-    //     case LabelType.Low:
-    //       return '' + value;
-    //     case LabelType.High:
-    //       return '' + value;
-    //     default:
-    //       return '' + value;
-    //   }
-    // }.
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return '' + value;
+        case LabelType.High:
+          return '' + value;
+        default:
+          return '' + value;
+      }
+    }
   };
   items = [
     { value: 1, legend: '#F44336' },
