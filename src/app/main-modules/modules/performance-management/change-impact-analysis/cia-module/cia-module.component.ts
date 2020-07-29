98a4166fdea854d2e211778161c27158259012f1
karan.dubey <karan.dubey@192.168.29.189>
2020-07-29 14:29:53 +0530

    PM CIA Module asset files.

--- src/app/main-modules/modules/performance-management/change-impact-analysis/cia-module/cia-module.component.ts
+++ src/app/main-modules/modules/performance-management/change-impact-analysis/cia-module/cia-module.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cia-module',
   templateUrl: './cia-module.component.html',
-  styleUrls: ['./cia-module.component.scss']
+  styleUrls: ['./cia-module.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CiaModuleComponent implements OnInit {
-
+  frequencyGroup = "Per Day";
+  public frequencyList: any[] = [
+    { 'name': 'Custom' },
+    { 'name': 'JC Circle Level' }
+  ];
   constructor() { }
 
   ngOnInit(): void {
