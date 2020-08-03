09640753de11a61664c01efea4afa45a704bb945
Abhishek6 C (Consultant) <Abhishek6.C@ril.com>
2020-08-03 19:12:19 +0530

    overshooting cell till date

--- src/app/main-modules/work-orders/rf-oc-workorders/category-wise-wo-listing/overshooting-cell/overshooting-cell.component.ts
+++ src/app/main-modules/work-orders/rf-oc-workorders/category-wise-wo-listing/overshooting-cell/overshooting-cell.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClient } from "@angular/common/http";
+import { AgGridModule } from 'ag-grid-angular';
+import { GridOptions, GridCore, GridApi, ColumnApi, } from "@ag-grid-community/all-modules";
+
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+//import { WoRanPopupComponent} from "./ran-popup/wo-ran-popup/wo-ran-popup.component"
 
 @Component({
   selector: 'app-overshooting-cell',
@@ -6,10 +13,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overshooting-cell.component.scss']
 })
 export class OvershootingCellComponent implements OnInit {
+  public gridApi;
+  public gridCore: GridCore;
+  public gridOptions: GridOptions;
+  //public rowData: any;
+  public columnDefswo;
+  public rowDatawo;
+  //public columnrt;
+  public rowrt;
+  constructor(public flexlayout: FlexLayoutModule, private http: HttpClient) {
 
-  constructor() { } 
+  }
 
   ngOnInit(): void {
+
+    this.gridOptions = <GridOptions>{};
+    //this.httpClientRowData();
+  
+    this.createColumndata();
+    this.createRowdata();
   }
+  private createColumndata() {
+this.columnDefswo = [
+
+  {
+    headerName: "Date",
+    field: "date",
+   width: 320
+   
+  }, {
+    headerName: "Reason for Reassignmenet",
+    field: "reasonforreassignment",
+    width: 320
+   
+  }, {
+    headerName: "Remarks",
+    field: "remarks",
+    width: 320
+    
+  }
+]
+  }
+
+   
+
+
+  
+  private createRowdata() {
+    this.http.get("assets/data/layers/randata.json")
+      .subscribe(data => {
+        console.log(data);
+        this.rowrt = data;
+    });
+  }
+
+  
 
 }
--- src/app/main-modules/work-orders/rf-oc-workorders/rf-oc-workorders.module.ts
+++ src/app/main-modules/work-orders/rf-oc-workorders/rf-oc-workorders.module.ts
@@ -51,12 +51,14 @@ import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { RfOcWorkordersModuleRoutingModule } from './rf-oc-workorders-routing.module';
 import { SectorMisalignmentComponent } from './category-wise-wo-listing/sector-misalignment/sector-misalignment.component';
-import { OvershootingCellComponent } from './category-wise-wo-listing/overshooting-cell/overshooting-cell.component';
+//import { OvershootingCellComponent } from './category-wise-wo-listing/overshooting-cell/overshooting-cell.component';
+import { WoRanPopupComponent } from './category-wise-wo-listing/overshooting-cell/ran-popup/wo-ran-popup/wo-ran-popup.component';
 
 @NgModule({
   declarations: [
     SectorMisalignmentComponent,
-    OvershootingCellComponent
+    
+    WoRanPopupComponent
   ], 
   imports: [
     RfOcWorkordersModuleRoutingModule,
