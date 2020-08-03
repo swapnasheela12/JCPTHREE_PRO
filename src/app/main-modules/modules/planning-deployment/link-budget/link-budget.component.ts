6d3dd0aac08978296a1cd78f3f96b6a74b7a8907
karan.dubey <karan.dubey@192.168.29.189>
2020-08-03 15:32:41 +0530

    Planning and Deployment 5G link budget asset files.

--- src/app/main-modules/modules/planning-deployment/link-budget/link-budget.component.ts
+++ src/app/main-modules/modules/planning-deployment/link-budget/link-budget.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-link-budget',
   templateUrl: './link-budget.component.html',
-  styleUrls: ['./link-budget.component.scss']
+  styleUrls: ['./link-budget.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class LinkBudgetComponent implements OnInit {
 
