7c3290f5ddf5ad9973859c92a86aac33f06ef006
Abhishek6 C (Consultant) <Abhishek6.C@ril.com>
2020-08-17 18:18:27 +0530

    updated spider view popups

---
+++ src/app/core/components/ag-grid-renders/genhelpicon.component.ts
@@ -0,0 +1,64 @@
+import { Component, ViewEncapsulation, Inject, OnInit } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ICellRendererParams, IAfterGuiAttachedParams } from 'ag-grid';
+
+import { MatDialog } from '@angular/material/dialog';
+import { AgGridModule } from 'ag-grid-angular';
+import { GridOptions, GridCore, SelectionChangedEvent, GridApi } from 'ag-grid-community';
+import { HttpClient } from '@angular/common/http';
+import { MatDialogRef } from "@angular/material/dialog";
+import { QuestionPopupComponent } from '../../../../app/modules/components/capacity/question-popup/question-popup.component'
+
+@Component({
+  selector: 'app-genhelpicon',
+  template: `<div *ngIf="kpis"  class="icon-help icon-button" id="helpicon-render" (click)="openDialoghelp()">
+  <div class="zmdi zmdi-help"></div>
+</div>`,
+  styles: [` .icon-button {
+    float: right;
+    padding: 10px 11px;
+    text-align: center;
+    width: 50px !important;
+    height: 50px !important;
+    border-radius: 25px;
+    background: none;
+
+}
+
+.zmdi-help {
+    font-size: 20px;
+    color:#777777;
+}`  ]
+})
+export class GenhelpiconComponent implements OnInit {
+
+  constructor(public dialog: MatDialog, public matDialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+  params;
+  public kpis;
+  public showGlobalOperation: Boolean = false;
+
+  refresh(params?: any): boolean {
+    return true;
+  }
+
+
+ 
+  agInit(params): void {
+    this.params = params;
+    this.kpis = this.params.data.kpis === "Average RRC Connected Users" || this.params.data.kpis === "AirMAC Cell DL Traffic (GB)" || this.params.data.kpis === "IP Throughput (Mbps)" 
+
+    };
+    
+    openDialoghelp(): void {
+      const dialogRef = this.dialog.open(QuestionPopupComponent, {
+        width: "550px",
+        panelClass: "material-dialog-container",
+       
+      });
+  
+    
+    };
+  }
