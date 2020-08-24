735f40e7e6dca118124cfff04f98af07d4b65580
Swapnasheela1.K <Swapnasheela1.K@ril.com>
2020-08-24 11:48:21 +0530

    cia conflict and qustionpop error resolve,custom legends layers

--- src/app/main-modules/modules/performance-management/my-performance-reports/my-performance-reports.component.ts
+++ src/app/main-modules/modules/performance-management/my-performance-reports/my-performance-reports.component.ts
@@ -1,55 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { GridOptions } from 'ag-grid-community';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { GridOptions, GridApi } from 'ag-grid-community';
 import * as moment from 'moment';
 import { HttpClient } from '@angular/common/http';
+import { DataSharingService } from 'src/app/_services/data-sharing.service';
+import { dropDownThreeDotRendererComponent } from './../../../../core/components/ag-grid-renders/dropDownThreeDot-renderer.component';
+import { Router } from '@angular/router';
+import { CommonDialogModel, CommonPopupComponent } from 'src/app/core/components/commanPopup/common-popup/common-popup.component';
+import { MatDialog } from '@angular/material/dialog';
 
 const HEADER_PERFORMANCE_REPORTS = [
   {
-    headerName: "Name",
-    field: "status",
-    width: 180,
-    // cellRenderer: 'statusFlagRenderer',
+    headerName: "",
+    width: 30,
     pinned: 'left',
     checkboxSelection: function(params) {
-      return params.columnApi.getRowGroupColumns().length === 0;
-    },
-    headerCheckboxSelection: function(params) {
       return params.columnApi.getRowGroupColumns().length === 0;
     },
     cellClass: 'lock-pinned',
   },
   {
+    headerName: "Name",
+    field: "name",
+    width: 220,
+    pinned: 'left',
+    cellClass: 'lock-pinned',
+  },
+  {
     headerName: "Type",
-    field: 'createdDate',
-    width: 160
+    field: 'type',
+    width: 180
   },
   {
     headerName: "Generation",
-    field: 'createdDate',
-    width: 160,
+    field: 'generation',
+    width: 180,
   },
   {
     headerName: "Domain",
     field: "domain",
-    width: 120
+    width: 140
   },
   {
     headerName: "Vendor",
     field: "vendor",
-    width: 120
+    width: 140
   },
   {
     headerName: "Created By",
-    colId: 'CfirstName&ClastName',
-    valueGetter: function(params) {
-      return params.data.createrFirstName
-        +' '+params.data.createrLastName;
-    },
+    field: 'createdby',
     width: 160
   },
   {
     headerName: "Created Date",
-    field: 'createdDate',
+    field: 'createddate',
     width: 160,
     valueFormatter: function(params){
       return moment(params.value).format('LL')
@@ -57,57 +60,195 @@ const HEADER_PERFORMANCE_REPORTS = [
   },
   {
     headerName: "Progress",
-    field: 'createdDate',
     width: 160
   },
   {
     headerName: "Status",
-    field: "vendor",
-    width: 120
+    width: 120,
+    suppressSorting: true,
+    suppressMenu: true
   },
   {
     headerName: "File Size",
-    field: "vendor",
+    field: "filesize",
     width: 120
   },
   {
     headerName: "",
-    cellRenderer:'VerticaldotRenderer',
-    width: 70,
-    pinned: 'right'
-
+    suppressMenu: true,
+    suppressSorting: true,
+    width: 90,
+    colId: 'dots-id'
   }
 ];
+
+const PATHS = [
+  { changeImpactAnalysis: "JCP/Modules/Performance-Management/My-Performance-Reports/Change-Impact-Analysis"}
+]
 
 @Component({
   selector: 'app-my-performance-reports',
   templateUrl: './my-performance-reports.component.html',
-  styleUrls: ['./my-performance-reports.component.scss']
+  styleUrls: ['./my-performance-reports.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class MyPerformanceReportsComponent implements OnInit {
   public gridMyPerformanceColumnDefs: any[];
   public gridMyPerformanceRowData: any;
-  public gridMyPerformanceGridOptions: GridOptions;
+  public gridMyPerformanceGridOptions : GridOptions;
   public frameworkComponentsMyPerformanceReports;
+  show: any;
+  searchGrid = '';
+  public sidenavBarStatus;
+  public gridPinned = true;
+  private gridApi;
+  public gridColumnApi;
+  public title = 'My Performance Reports';
+  public url: string = "assets/data/modules/performance_management/my-performance-report/my-performance-report.json";
+  public rowSelection;
+  public frameworkComponentsReportBuilder;
+  private paginationPageSize = 10;
 
-  constructor(private http: HttpClient) { }
-  // my-performance-report.json
+  public fitColumns() {
+    if (this.gridMyPerformanceGridOptions.api && this.gridMyPerformanceRowData) {
+      setTimeout(() => {
+        this.gridMyPerformanceGridOptions.api.sizeColumnsToFit();
+      }, 500);
+    }
+  }
+
+  constructor(
+    private http: HttpClient,
+    public dialog: MatDialog,
+    private router: Router,
+    private datashare: DataSharingService
+  ) {
+    this.gridMyPerformanceGridOptions = <GridOptions>{};
+    this.frameworkComponentsReportBuilder = {
+      'VerticaldotRenderer': dropDownThreeDotRendererComponent
+    };
+    this.paginationPageSize = 10;
+    this.datashare.currentMessage.subscribe((message) => {
+      this.sidenavBarStatus = message;
+      // this.fitColumns();
+    });
+  }
 
   ngOnInit(): void {
     this.createColumnDefs();
     this.getMyPerformanceReportsData();
   }
 
-  private createColumnDefs() {
-    this.gridMyPerformanceColumnDefs = HEADER_PERFORMANCE_REPORTS;
+  get gridAPI(): GridApi {
+    return this.gridApi;
   }
 
-  private getMyPerformanceReportsData() {
-    this.http.get("assets/data/modules/performance_management/my-performance-report/my-performance-report.json")
+  get PaginationPageSize(): number {
+    return this.paginationPageSize;
+  }
+
+  public onReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    // setTimeout(() => {
+    //   this.fitColumns();
+    // }, 500);
+  }
+
+  toggleSearch() {
+    this.show = !this.show;
+  };
+
+  public createColumnDefs() {
+    this.gridMyPerformanceColumnDefs = HEADER_PERFORMANCE_REPORTS;
+    this.gridMyPerformanceColumnDefs[9].cellRenderer = this.shareStatus;
+    this.gridMyPerformanceColumnDefs[8].cellRenderer = this.progressTaskFunc;
+    this.gridMyPerformanceColumnDefs[11].cellRenderer = 'VerticaldotRenderer';
+  }
+
+  defaultColDef = {
+    resizable: false,
+    suppressRowClickSelection: true,
+    suppressCellSelection: true
+  };
+  public getMyPerformanceReportsData() {
+    this.http.get(this.url)
       .subscribe(data => {
-        this.gridMyPerformanceRowData = data;
+        this.gridMyPerformanceRowData = data;       
     });
   }
 
+  onFilterChanged(value) {
+    this.gridMyPerformanceGridOptions.api.setQuickFilter(value);
+  };
+
+  shareStatus(params) {
+    var data = params.data;
+    if (!params.data)
+      return '';
+    var status = params.data.status;
+    var barColor = '';
+    if (status == "Shared") {
+      barColor = '#4188de';
+      var template = '<div class="shared_val" fxLayout="row" fxLayoutAlign="space-between center"> <div class="shared_title">' + status + '</div> <div class="shared_count">+' + params.data.sharecount + '</div> </div>'
+    } else {
+      barColor = '#828282';
+      var template = '<div fxLayout="row" fxLayoutAlign="space-between center"> <div class="shared_title">-</div> </div>'
+    }
+    ;
+    return template;
+  };
+
+  progressTaskFunc(params) {
+    var taskcompletion = params.data.progressby;
+    var taskprogress = params.data.progressbar;
+
+    var template1 = '<div class="jcp-two-lines-progress">' + '<div class="values">' + taskcompletion + '</div>' +
+      ' <div class="progress"> <div class="progress-bar bg-success" style="width:' + taskprogress + '%"></div> </div></div>';
+
+    var template2 = '<div class="jcp-two-lines-progress">' + '<div class="values">' + taskcompletion + '</div>' +
+      ' <div class="progress"> <div class="progress-bar bg-warning" style="width:' + taskprogress + '%"></div> </div></div>';
+
+    var template3 = '<div class="jcp-two-lines-progress">' + '<div class="values">' + taskcompletion + '</div>' +
+      ' <div class="progress"> <div class="progress-bar bg-danger" style="width:' + taskprogress + '%"></div> </div></div>';
+    if (taskcompletion == "Generated") {
+      return template1;
+    } else if (taskcompletion == "#5 in Queue") {
+      return template2;
+    } else {
+      return template3;
+    }
+  }
+
+  onRowClicked(evt) {
+      if (evt.event.target.localName == 'mat-icon') {
+        return false;
+      }
+      if(evt.data.type =='Pre-Post Report'){
+        this.router.navigate([PATHS[0].changeImpactAnalysis]);
+      }
+  }
+
+  onCellClicked(evt) {
+    if (evt.node.data) {
+      console.log(evt.column.colDef.colId)
+      if (evt.column.colDef.colId == 'dots-id') {
+        return false;
+      } else {
+        this.router.navigate([PATHS[0].changeImpactAnalysis]);
+      }
+    }
+  }
+
+  openUpdateDialog(): void {
+    const message = `Are you sure you want to update the Admin Settings?`;
+    const image = 'warning';
+    const snackbarMode = 'warning';
+    const snackbarText = 'Admin Settings Updated Successfully.';
+    const dialogData = new CommonDialogModel("Warning!", message, image, snackbarMode, snackbarText);
+    const dialogRef = this.dialog.open(CommonPopupComponent, {
+      data: dialogData
+    });
+  }
 }
