6dc541dfe79b93379e85baefb6c7ab12c3a54052
Pooja1 Das <Pooja1.Das@ril.com>
2020-09-28 14:43:54 +0530

    Resolved integration fixes

--- src/app/main-modules/main-modules.module.ts
+++ src/app/main-modules/main-modules.module.ts
@@ -3,7 +3,7 @@ import { colorDropdownRendererComponent } from './../core/components/ag-grid-ren
 import { KpiSettingsComponent } from './main-layer/kpi-details/kpi-settings/kpi-settings.component';
 import { CoreModule } from './../core/core.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { MainModulesRoutingModule } from './main-modules-routing.module';
 
 //Angular Material Components
@@ -91,6 +91,14 @@ import { SelectedLayerMenuComponent } from './main-layer/selected-layer-menu/sel
 import { SpiderComponent } from './main-layer/sites/outdoor/spider/spider.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { KpiComponent } from './main-layer/sites/outdoor/spider/popup/kpi/kpi.component';
+import { CandidatesComponent } from '../modules/components/properties/candidates/candidates.component';
+import { NominalViewComponent } from './main-layer/sites/outdoor/spider/nominal-view/nominal-view.component';
+import { TacNetworkDialogComponent } from './main-layer/location-and-boundaries/Network/TAL/tal-dialog/tal-dialog.component';
+import { DenseUrbanDialogComponent } from './main-layer/location-and-boundaries/Morphology/DenseUrban/dense-urban-dialog/dense-urban-dialog.component';
+import { TownBoundaryDialogComponent } from './main-layer/location-and-boundaries/CensusData/TownBoundary/town-boundary-dialog/town-boundary-dialog.component';
+import { ZonesJioDialogComponent } from './main-layer/location-and-boundaries/Jio/Zones/zones-dialog/zones-dialog.component';
+import { NominalMacroDialogComponent } from './main-layer/sites/nominal/macro-dialog/macro-dialog.component';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   declarations: [
@@ -122,7 +130,14 @@ import { KpiComponent } from './main-layer/sites/outdoor/spider/popup/kpi/kpi.co
     ScreenshotPreviewComponent,
     SpiderComponent,
     SpiderViewComponent,
-    KpiComponent
+    KpiComponent,
+    CandidatesComponent,
+    NominalViewComponent,
+    TacNetworkDialogComponent,
+    DenseUrbanDialogComponent,
+    TownBoundaryDialogComponent,
+    ZonesJioDialogComponent,
+    NominalMacroDialogComponent
   ],
   imports: [
     CommonModule,
@@ -184,14 +199,11 @@ import { KpiComponent } from './main-layer/sites/outdoor/spider/popup/kpi/kpi.co
     PortalModule,
     ScrollingModule,
     NgxMatSelectSearchModule,
-    PerformanceManagementModule,
-    PlanningDeploymentModule,
-    RfOcWorkordersModule,
-
-
+    ColorPickerModule
   ], providers: [
     SideNavService,
     MarkerService,
+    DatePipe,
     {
       provide: MatDialogRef,
       useValue: {}
