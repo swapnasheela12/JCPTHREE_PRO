5ebd4fcaaa9e3fd7f103ece80f78e9f40afb21e0
Swapnasheela1.K <Swapnasheela1.K@ril.com>
2020-10-01 23:24:12 +0530

    nominal

--- src/app/main-modules/main-layer/sites/nominal/macro-nominal.service.ts
+++ src/app/main-modules/main-layer/sites/nominal/macro-nominal.service.ts
@@ -37,10 +37,10 @@ export class MacroNominalService {
   stage;
   _bounds;
   zoomLevel;
-   _assetQueue = null;
-   _colors = ['#757584', '#92D050', '#8C6900', '#006838', '#00506A', '#00ADEE', '#5900B2', '#0D47A1'];
-   _siteImagePath = 'assets/images/Layers/';
-   _plannedSiteImageManifest = [{
+  _assetQueue = null;
+  _colors = ['#757584', '#92D050', '#8C6900', '#006838', '#00506A', '#00ADEE', '#5900B2', '#0D47A1'];
+  _siteImagePath = 'assets/images/Layers/';
+  _plannedSiteImageManifest = [{
     id: 'smallcellpetal',
     src: this._siteImagePath + '3-1.svg',
     type: createjs.LoadQueue.IMAGE
@@ -95,41 +95,9 @@ export class MacroNominalService {
     canvasLayer.on("layer-mounted", function () { });
 
     canvasLayer.on("layer-render", function () {
-       
+
       let customLayerThis: any = this;
       componentRef.sitesNominalLayerMap(customLayerThis);
-      // let ctx = resizeContainer.bind(this)();
-      // ctx.fillStyle = "rgba(255,116,0, 0.5)";
-      // let bounds = this._map.getBounds();
-      // for (let i = 0; i < data.length; i++) {
-      //   let d = data[i].latlng;
-      //   if (bounds.contains([d[0], d[1]])) {
-      //     let dot = this._map.latLngToContainerPoint([d[0], d[1]]);
-      //     ctx.beginPath();
-      //     ctx.arc(dot.x, dot.y, 3, 0, Math.PI * 2);
-      //     ctx.fill();
-      //     ctx.closePath();
-      //   }
-      // }
-
-      // let zoomlevel = this._map.getZoom();
-      // if (zoomlevel < 10) {
-      //   if (this._map.hasLayer(this.stateLayer)) {
-      //     console.log("333333333");
-      //     this._map.removeLayer(this.stateLayer);
-      //   } else {
-      //     console.log("no point layer active");
-      //   }
-      // }
-      // if (zoomlevel >= 10) {
-      //   if (this._map.hasLayer(this.sitesNominalLayer)) {
-      //     console.log("layer already added");
-      //   } else {
-      //     this._map.addLayer(this.sitesNominalLayer);
-      //   }
-      // }
-      // console.log("Current Zoom Level =" + zoomlevel)
-
     });
 
     canvasLayer.on("layer-beforedestroy", function () { });
@@ -145,11 +113,7 @@ export class MacroNominalService {
       for (let index = 0; index < this.selectedLayerArrList.length; index++) {
         const ele = this.selectedLayerArrList[index];
         if (ele.eventName == "sites-nominal-macro-macro4G") {
-          console.log("got it all amll ESC");
-          // this.boundariesData();
-          // this.makeCapitalMarkers(this._map);
           this.boundariesData();
-          // return canvasLayer.addTo(this.map);
           return canvasLayer.addTo(this.map);
         }
       }
@@ -224,23 +188,17 @@ export class MacroNominalService {
       //EMPTY THE CANVAS
       this.container.removeAllChildren();
 
-      // if (this.addtionalsector == 'Planned') {
-      //   let siteCenterDotColor = "rgb(0,15,255)";
-      // } else {
-      //   let siteCenterDotColor = "#06C1FF";
-      // }
-
       //CENTER DOT OF THE SHAPE
 
       // let siteCenterDot = this.getPointGraphics(siteCenterDotColor);
-      // let shadow = new createjs.Shadow("rgba(0,0,0,0.2)", 1, 2, 5);
-      
-     
+      let shadow = new createjs.Shadow("rgba(0,0,0,0.2)", 1, 2, 5);
+      let bounds = this.map.getBounds();
+
       for (const site in data) {
         let siteInner = data[site];
         let latlng = L.latLng(siteInner[0].latitude, siteInner[0].longitude);
 
-        let bounds = this.map.getBounds();
+        
 
         // PLACING THE COORDINATES
         if (!(bounds.contains(latlng))) continue;
@@ -252,62 +210,60 @@ export class MacroNominalService {
 
         let siteContainer = new createjs.Container();
         siteContainer.cursor = 'pointer';
-        // siteContainer.shadow = shadow;
+        siteContainer.shadow = shadow;
         siteContainer.x = centerPoint.x;;
         siteContainer.y = centerPoint.y;;
         siteContainer.scaleX = scaleMatrix;
         siteContainer.scaleY = scaleMatrix;
         siteContainer.name = siteInner[0].sapid;
 
-        this._assetQueue = new createjs.LoadQueue(false,null,true);
-        
-        let labelsContainer = new createjs.Container();
+        this._assetQueue = new createjs.LoadQueue(false, null, true);
+        this._assetQueue.loadManifest(this._plannedSiteImageManifest, true);
+        if (this.zoomLevel >= 12) {
 
-        for (let band in siteInner) {
-          let bandInner = siteInner[band];
-          let outerRadius = 75;
-          let innerRadius = 15;
+          for (let band in siteInner) {
+            let bandInner = siteInner[band];
 
-          let petalLength = bandInner.siteArray.length;
-          let shadow = new createjs.Shadow("rgba(0,0,0,0.2)", 1, 2, 5);
-          for (let j = 0, jCount = petalLength; j < jCount; j++) {
-            let cell = bandInner.siteArray[j];
-            // let angle = cell.azimuth - (20 / 2);
-            let id = cell.cellStatus == "Landmark Coverage" ? "smallcellpetalyellow" : "smallcellpetal";
+            let petalLength = bandInner.siteArray.length;
+            for (let j = 0, jCount = petalLength; j < jCount; j++) {
+              let cell = bandInner.siteArray[j];
+              let id = cell.cellStatus == "Landmark Coverage" ? "smallcellpetalyellow" : "smallcellpetal";
 
-            //  let siteImage = this._siteImagePetal.clone()
-            console.log(id, "id");
-            console.log(this._assetQueue,"_assetQueue");
-            // let siteImage = new createjs.Bitmap(_assetQueue.getResult(id));
-            // let siteImage = new createjs.Bitmap("assets/images/logo/favicon.ico/android-icon-36x36.png");
-            let ImageNominal = this._assetQueue.getResult("smallcellpetal");
-            console.log(ImageNominal,"ImageNominal");
-            
-            let siteImage = new createjs.Bitmap(ImageNominal);
-            console.log(siteImage,"siteImage");
-            
-            siteImage.scaleX = 3.0;
-            siteImage.scaleY = 3.0;
-            siteImage.regX = 13.5;
-            siteImage.regY = 27;
-            // siteImage.rotation = angle;
-            siteImage['latlng'] = latlng;
-            siteImage['data'] = band;
-            siteImage['current'] = cell;
+              let siteImage;
+              if (cell.cellStatus == "Landmark Coverage") {
+                siteImage = new createjs.Bitmap("assets/images/Layers/3.svg");
+              } else {
+                siteImage = new createjs.Bitmap("assets/images/Layers/3-1.svg");
+              }
 
-            siteContainer.addChild(siteImage);
+              siteImage.scaleX = 5.0;
+              siteImage.scaleY = 5.0;
+              siteImage.regX = 13.5;
+              siteImage.regY = 27;
+              siteImage['latlng'] = latlng;
+              siteImage['data'] = band;
+              siteImage['current'] = cell;
 
-            siteImage.addEventListener('click', function (evt) {
-              let target = evt;
-            });
 
-            
+
+              siteImage.addEventListener('click', function (evt) {
+                console.log(evt, "evt");
+
+                let target = evt;
+                // this.spiderViewFeature(evt); 
+              });
+              siteImage.addEventListener('click', function (evt) {
+                console.log(evt, "evt");
+
+                let target = evt;
+                // this.spiderViewFeature(evt); 
+              });
+
+              siteContainer.addChild(siteImage);
+
+            }
 
           }
-
-        }
-
-        if (this.zoomLevel >= 12) {
 
           let label = new createjs.Text(siteInner[0].sapid, "bold 60px Lato-Medium", "#FFFFFF");
           label.textAlign = 'center';
@@ -315,7 +271,7 @@ export class MacroNominalService {
           label.y = (scaleMatrix * 200) / this.pixelRatio;
 
           let outline = label.clone();
-          //outline.shadow = shadow;
+          outline.shadow = shadow;
           outline.color = '#000000';
           siteContainer.addChild(label, outline);
 
@@ -323,81 +279,22 @@ export class MacroNominalService {
 
         bounds.extend(latlng);
         this.container.addChild(siteContainer);
-        this._assetQueue.loadManifest(this._plannedSiteImageManifest, true);
 
       }
 
-      this.container.alpha = 1;
+      // this.container.alpha = 1;
 
       //PUSH THE SHAPES SAVED IN CONTAINER AND DISPLAY IT 
       this.stage.addChild(this.container);
+      // this.stage.update();
+
+      this._bounds = bounds;
+      this.container.alpha = 1;
       this.stage.update();
-     
-      // _assetQueue.loadManifest({
-      //   id: 'code',
-      //   src:  _siteImagePath + '3.svg',
-      //   type: createjs.AbstractLoader.IMAGE
-      // },true);
-      
+      // console.timeEnd(this.container.name);
+
     }
 
-  }
-
-  private spiderViewFeature(e) {
-    const layer = e.target;
-    console.log(layer, "layer");
-  }
-
-  nodeOnMouseClick(event) {
-    this.map.setView(event.target.latlng, 17);
-    this.loadSpiderViewComponent(event);
-  }
-
-  loadSpiderViewComponent(event) {
-    let biggerNodeData = {};
-    biggerNodeData['currentbands'] = event.target.site;
-    biggerNodeData['sector'] = event.target.sector;
-
-    // this.referenceComp.targetElementSpiderView.clear();
-
-    // if (this.dataSharingService) {
-    //   this.dataSharingService.sendDataToSpider(biggerNodeData);
-    // }
-    // else {
-    //   throw new Error("Data sharing service not found. Please add one.");
-    // }
-
-    // if (this.componentFactoryResolver) {
-    //   let spiderComponent = this.componentFactoryResolver.resolveComponentFactory(SpiderComponent);
-    //   this.referenceComp.targetElementSpiderView.createComponent(spiderComponent);
-    // }
-    // else {
-    //   throw new Error("Dynamic component loader not found. Please include resolveComponentFactory module from the ng core.");
-    // }
-  }
-  //ON MOUSE OVER OF SHAPE(FAN)
-  nodeOnMouseOver(event) {
-    let target = event.target;
-    let c = event.currentTarget;
-    target.alpha = 1;
-    target.graphics.strokeStyleCommand.width = 4;
-    this.stage.update();
-    if (c.sector.sitebandtype !== undefined && c.sector.sitebandtype !== null) {
-      let band = (c.sector.sitebandtype == 'site2300') ? 2300 : (c.sector.sitebandtype == 'site1800') ? 1800 : 850;
-      let template = "";
-      template += `<div class="layout-row"><span class="prefix">PCI : </span> <span class="value">${(c.sector.pci)} </span></div>`;
-      template += `<div class="layout-row"><span class="prefix">Band : </span> <span class="value">${(band)} MHz</span></div>`;
-      this._simplePopup.setLatLng(c.latlng).setContent(template).openOn(this.map);
-    }
-  }
-
-  //ON MOUSE OUT OF SHAPE(FAN)
-  nodeOnMouseOut(event) {
-    let target = event.target;
-    target.alpha = 0.8;
-    target.graphics.strokeStyleCommand.width = 1;
-    this.stage.update();
-    this.map.closePopup();
   }
 
   getPopup() {
@@ -409,60 +306,6 @@ export class MacroNominalService {
     });
     return popup;
   }
-
-  getPointGraphics(color) {
-    let g = new createjs.Graphics();
-    g.setStrokeStyle(1);
-    g.beginStroke(createjs.Graphics.getRGB(0, 0, 0));
-    g.beginFill(color);
-    g.drawCircle(0, 0, 10);
-    return g;
-  };
-
-  getSelectionGraphics(strokecolor, color) {
-    let g = new createjs.Graphics();
-    g.setStrokeStyle(5);
-    g.beginStroke(strokecolor);
-    g.beginFill(color);
-    g.drawCircle(0, 0, 90);
-    return g;
-  };
-
-  pieGenerator(pie_x, pie_y, startAngle, endAngle, radius1, radius2, carrierInnerRadius, carrierStatus, fillColor, lineColor, lineThickness) {
-    let newAngles = (endAngle - startAngle) / 2;
-    let newAngle = startAngle + newAngles;
-
-    let g = new createjs.Graphics();
-    let strokeStyleCommand = g.setStrokeStyle(lineThickness).command;
-    g['strokeStyleCommand'] = strokeStyleCommand;
-
-    g.beginFill(fillColor);
-    g.beginStroke(lineColor);
-    g.arc(pie_x, pie_y, radius1, this.toRad(startAngle), this.toRad(endAngle), false);
-    g.lineTo(pie_x + Math.cos(this.toRad(endAngle)) * radius2, pie_y + Math.sin(this.toRad(endAngle)) * radius2);
-    g.arc(pie_x, pie_y, radius2, this.toRad(endAngle), this.toRad(startAngle), true);
-    g.closePath();
-    g.endFill();
-
-
-    if (carrierStatus) {
-      g.setStrokeStyle(1);
-      g.moveTo(pie_x + Math.cos(this.toRad(newAngle)) * carrierInnerRadius, pie_y + Math.sin(this.toRad(newAngle)) * carrierInnerRadius);
-      g.lineTo(pie_x + Math.cos(this.toRad(newAngle)) * radius1, pie_y + Math.sin(this.toRad(newAngle)) * radius1);
-      g.closePath();
-      g.beginStroke("#CD4B5B");
-    }
-    return g;
-  }
-
-  toRad(angle) {
-    return (angle - 90) * Math.PI / 180;
-  }
-
-  // removeLayerFromMap() {
-  //   this.canvasLayerObj.remove();
-  // }
-
 
   resizeContainer = function (customeLayerThisRef) {
     let canvas = customeLayerThisRef.getContainer();
